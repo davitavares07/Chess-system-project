62fab6dbc5ebf325414f74afee260ae0ea1183ce
Alberto Davi Gomes Tavares <alberto.tavares@fiocruz.br>
2023-10-26 10:30:32 -0300

    Board Exception e correção defensiva

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -27,7 +27,8 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(4,1));
+		board.placePiece(new King(board, Color.WHITE), new Position(0,1));
 		board.placePiece(new King(board, Color.BLACK), new Position(5,4));
 	}
 }
