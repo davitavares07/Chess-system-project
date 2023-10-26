68ec70998cd28fd835c17020131b195054f10e86
Alberto Davi Gomes Tavares <alberto.tavares@fiocruz.br>
2023-10-26 10:08:40 -0300

    Pieces on the board

---
+++ chess-system/src/chess/pieces/King.java
@@ -0,0 +1,16 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+
+public class King extends ChessPiece {
+
+	public King(Board board, chess.Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "K";
+	}
+}
