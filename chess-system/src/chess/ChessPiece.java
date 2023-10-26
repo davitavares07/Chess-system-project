fd8f2eaf84f8160eb1a4f90cf98a2357cf2d5fd3
Alberto Davi Gomes Tavares <alberto.tavares@fiocruz.br>
2023-10-26 09:48:23 -0300

    Chess Layer and print board

---
+++ chess-system/src/chess/ChessPiece.java
@@ -0,0 +1,20 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+
+	private Color Color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		Color = color;
+	}
+
+	public Color getColor() {
+		return Color;
+	}
+	
+	
+}
